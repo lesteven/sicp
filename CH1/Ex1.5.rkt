91734568e7349e3fa73b0d585cf4c6a17e2bfc9b
lesteven <le_steven@outlook.com>
2018-06-09 21:47:44 -0700

    add 1.5

---
+++ CH1/Ex1.5.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+#|
+  Subsitition model
+  - evalute body of procedure with corresponding arguments 
+
+  Applicative order evaluation
+  - evaluate all arguments, then apply operator
+
+  Normal-order evaluation
+  - fully expand and then reduce
+    - function calls are expanded before reducing arguments
+    - reduce only when value is needed
+
+  APE
+  - infinite loop b/c (p) never finish evaluating
+
+  NOE
+  - delay reduction of (p) until end, so does not need to evalute (p)
+|#
+
+
+(define hi 0)
+hi
+
+(define (p) (p))
+(define (test x y)
+  (if (= x 0) 0 y))
+
+(test 0 (p))
